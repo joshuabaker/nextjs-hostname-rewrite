ff3979b2f647d0ada2dc9c92aec124b14f221e1b
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2022-04-29 14:24:32 +0100

    Comment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: { locales: ["en"], defaultLocale: "en" },
+  i18n: { locales: ["en"], defaultLocale: "en" }, // This config breaks middleware rewrites
 };
 
 module.exports = nextConfig;
